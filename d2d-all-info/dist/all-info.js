4c442c7db6edf7745653cbc8ba6ddbff3bc11156
Alec <fernandes.alec@gmail.com>
2021-06-23 19:29:48 -0700

    added d2d-all-info npm package to repo

---
+++ d2d-all-info/dist/all-info.js
@@ -0,0 +1,4 @@
+import dharmaLists, { paliWords, thePath, justPaths } from "./dharma-info";
+import noDefault, { categoryWords, getRandomPaliWords, getCategoriesForDharmaQuiz, getRandomDharmaQuiz, shuffle } from "./dharma-functions";
+export default dharmaLists;
+export { paliWords, thePath, justPaths, noDefault, categoryWords, getRandomPaliWords, getCategoriesForDharmaQuiz, getRandomDharmaQuiz, shuffle };
